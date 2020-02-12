7842601f87718d78a2e2166cb65ff67aa66a89e7
itjianglihua <1026794650@qq.com>
2020-02-12 12:52:32 +0800

    新的

--- Base/getData.py
+++ Base/getData.py
@@ -1,5 +1,10 @@
 import yaml,os
+import base_dir
+
+
 class GetData:
     def get_yaml_data(self,yaml_name):
-        with open("../Data" + os.sep + yaml_name, "r")as f:
+        # with open("../Data" + os.sep + yaml_name, "r")as f:
+        with open(base_dir.BASE_DIR+"./Data"+ os.sep + yaml_name, "r")as f:
             return yaml.safe_load(f)
+
